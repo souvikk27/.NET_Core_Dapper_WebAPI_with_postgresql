2171fc226184b866c2df591e8088fb959743b17c
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2023-05-17 15:31:02 +0530

    Added package BCrypt

---
+++ Dapper.WebAPI/Helpers/PasswordEncrypt.cs
@@ -0,0 +1,11 @@
+﻿namespace Dapper.WebAPI.Helpers
+{
+    public class PasswordEncrypt
+    {
+        public string GeneratePasswordHash(string? password)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+            return passwordHash;
+        }
+    }
+}
