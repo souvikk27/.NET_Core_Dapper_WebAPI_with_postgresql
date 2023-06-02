d20b021cca750281fe7d8ca8698d52e750937529
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2023-06-02 18:40:39 +0530

    database context seed

--- Dapper.WebAPI/Seeds/ContextSeed.cs
+++ Dapper.WebAPI/Seeds/ContextSeed.cs
@@ -2,5 +2,12 @@
 {
     public class ContextSeed
     {
+        public static void SeedDefaultUser(IServiceProvider serviceProvider)
+        {
+            using(var scope = serviceProvider.CreateScope())
+            {
+
+            }
+        }
     }
 }
